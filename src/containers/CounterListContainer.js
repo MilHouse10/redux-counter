33ad4893f9ba2a8282aee9b77a2cedb8e017fdcb
MilHouse10 <alstjr_96@naver.com>
2021-06-10 15:43:09 +0900

    multi counter

--- src/containers/CounterListContainer.js
+++ src/containers/CounterListContainer.js
@@ -1,31 +1,30 @@
-import Counter from '../components/Counter';
+import CounterList from '../components/CounterList';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { getRandomColor } from '../utils';
 
 //store 안의 state 값을 porps로 연결해준다.
 const mapStateToProps = (state) => ({
-    color: state.colorData.color,
-    number: state.numberData.number
+    counters: state.counters
 });
 
 //액션 생성자를 사용하여 액션을 생성하고,
 //해당 액션을 dispatch하는 함수를 만든 후, 이를 props로 연결한다.
 
 const mapDispatchToProps = (dispatch) => ({
-    onIncrement: () => dispatch(actions.increment()),
-    onDecrement: () => dispatch(actions.decrement()),
-    onSetColor: () => {
+    onIncrement: (index) => dispatch(actions.increment(index)),
+    onDecrement: (index) => dispatch(actions.decrement(index)),
+    onSetColor: (index) => {
         const color = getRandomColor();
-        dispatch(actions.setColor(color));
+        dispatch(actions.setColor({index, color}));
     }
-});
+})
 
 //Counter 컴포넌트의 Container 컴포넌트
 //Counter 컴포넌트를 어플리케이션의 데이터 레이어와 묶는 역할을 한다.
-const CounterContainer = connect(
+const CounterListContainer = connect(
     mapStateToProps,
     mapDispatchToProps
-)(Counter);
+)(CounterList);
 
-export default CounterContainer;
+export default CounterListContainer;
